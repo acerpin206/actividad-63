5894aa2b8063e7bf30462022c5e541b207207606
acerpin206 <132546867+acerpin206@users.noreply.github.com>
2023-05-11 08:47:26 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+let b = 225 - input.lightLevel()
+led.setBrightness(b)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.forever(function () {
+	
+})
